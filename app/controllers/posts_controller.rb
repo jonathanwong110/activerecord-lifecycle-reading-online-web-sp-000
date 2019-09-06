30067138c361f90b32a7b5143c4118a9c3af3f65
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-06 21:57:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -23,6 +24,7 @@ class PostsController < ApplicationController
 	end
 
 	def edit
+	  @post = Post.find(params[:id])
 	end
 	
 	private
