ad5fe551afd055c178a45e2d5a304f47d1873a46
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-06 22:41:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,33 +12,18 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(post_params)
-	  if @post.valid?
-	    @post.save
-	    redirect_to post_path(@post)
-	   else
-	     render :new
-	   end
+	  @post = Post.new(params)
+	  @post.save
+	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(post_params)
-	  if @post.valid?
-	    @post.save
-	    redirect_to post_path(@post)
-	  else
-	    render :edit
-	  end
+	  @post.update(params.require(:post))
+	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
-	
-	private
-	  def post_params
-	    params.require(:post).permit(:title, :description)
-	  end
-	
 end
